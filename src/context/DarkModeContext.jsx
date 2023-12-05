4f7abb06c7be56e8c5ac950b9b99a3aca4dec427
이동익/글로벌개발팀 <dongik.lee@musinsa.com>
2023-12-05 22:24:54 +0900

    feat: darkmode localstorage 추가

--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -1,10 +1,31 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const DarkModeContext = createContext();
 
 export function DarkModeProvider({ children }) {
   const [darkMode, setDarkMode] = useState(true);
-  const toggleDarkMode = () => setDarkMode((prev) => !prev);
+  const toggleDarkMode = () => {
+    if (!darkMode) {
+      localStorage.setItem("darkMode", "darkMode");
+    } else {
+      localStorage.setItem("darkMode", "lightMode");
+    }
+    setDarkMode((prev) => !prev);
+  };
+
+  useEffect(() => {
+    const darkModeStorage = localStorage.getItem("darkMode");
+    if (!darkModeStorage && darkMode) {
+      localStorage.setItem("darkMode", "darkMode");
+    }
+
+    if (darkModeStorage === "darkMode") {
+      setDarkMode(true);
+    } else {
+      setDarkMode(false);
+    }
+  }, []);
+
   return (
     <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>
       {children}
